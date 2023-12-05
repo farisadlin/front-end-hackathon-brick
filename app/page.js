303677da1582f0f9d05a0492c6d4b9ee9b3f7200
farisadlin <farisnura@gmail.com>
2023-12-06 03:42:16 +0700

    feat: changed the UI for better UX

--- app/page.js
+++ app/page.js
@@ -5,39 +5,68 @@ import axios from 'axios';
 
 export default function Home() {
   const [input, setInput] = useState('');
-  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [isSending, setIsSending] = useState(false);
 
   const sendMessage = async () => {
+    if (!input.trim()) return;
+    
+    setIsSending(true);
+
     try {
       const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/chat`, {
         text: input
       });
 
-      setMessage(response.data.response)
-
+      setMessages([...messages, { type: 'user', text: input }, { type: 'bot', text: response.data.response }]);
     } catch (error) {
       console.error('Error sending message:', error);
-      // Handle the error appropriately - maybe show it in the UI
+      setMessages([...messages, { type: 'error', text: 'Error sending message.' }]);
     }
 
     setInput('');
+    setIsSending(false);
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && !isSending) {
+      sendMessage();
+    }
+  }
+
   return (
-    <div>
-      <h1>Chat with ChatGPT</h1>
-      <div>
-          <p>
-            {message}
-          </p>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4 text-center">Chat with Brick</h1>
+      <div className="chat-window bg-neutral p-4 shadow rounded-lg h-[80vh] overflow-y-auto">
+        {messages.map((msg, index) => (
+          <div key={index} className={`chat-message ${msg.type === 'user' ? 'text-right' : ''}`}>
+            <span className={`inline-block px-3 py-2 rounded-lg max-w-xs m-1 text-white ${msg.type === 'user' ? 'bg-accent' : 'bg-neutral border border-gray-500'}`}>
+              {msg.text}
+            </span>
+          </div>
+        ))}
       </div>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Type your message..."
-      />
-      <button onClick={sendMessage}>Send</button>
+      <div className="mt-4 flex">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={handleKeyPress}
+          placeholder="Type your message..."
+          autoFocus
+          className="input input-bordered w-full"
+          disabled={isSending}
+        />
+        <button onClick={sendMessage} className={`btn btn-primary ml-2 ${isSending && "cursor-wait"} text-white`} disabled={isSending}>
+          {isSending ? <Spinner /> : 'Send'}
+        </button>
+      </div>
     </div>
+  );
+}
+
+function Spinner() {
+  return (
+      <span class="loading loading-spinner loading-lg cursor-wait"></span>
   );
 }
