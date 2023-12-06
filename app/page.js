ac4076f15d20030226acd6abf7d0184521ee9e7a
farisadlin <farisnura@gmail.com>
2023-12-06 10:18:12 +0700

    feat: trim the \n word

--- app/page.js
+++ app/page.js
@@ -8,6 +8,13 @@ export default function Home() {
   const [messages, setMessages] = useState([]);
   const [isSending, setIsSending] = useState(false);
 
+  function breakDownText(text) {
+    // Split the text by newline characters
+    const parts = text.split('\n').map(part => part.trim()).filter(part => part !== '');
+  
+    return parts;
+  }
+  
   const sendMessage = async () => {
     if (!input.trim()) return;
     
@@ -18,7 +25,14 @@ export default function Home() {
         text: input
       });
 
-      setMessages([...messages, { type: 'user', text: input }, { type: 'bot', text: response.data.response }]);
+      const formattedResponse = breakDownText(response.data.response);
+
+      // Adding each part as a separate message
+      formattedResponse.forEach(msg => {
+        setMessages(prevMessages => [...prevMessages, { type: 'bot', text: msg }]);
+      });
+
+      // setMessages([...messages, { type: 'user', text: input }, { type: 'bot', text: response.data.response }]);
     } catch (error) {
       console.error('Error sending message:', error);
       setMessages([...messages, { type: 'error', text: 'Error sending message.' }]);
@@ -37,14 +51,17 @@ export default function Home() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4 text-center">Chat with Brick</h1>
+    
+      {/* Chat window for displaying messages */}
       <div className="chat-window bg-neutral p-4 shadow rounded-lg h-[80vh] overflow-y-auto">
-        {messages.map((msg, index) => (
-          <div key={index} className={`chat-message ${msg.type === 'user' ? 'text-right' : ''}`}>
-            <span className={`inline-block px-3 py-2 rounded-lg max-w-xs m-1 text-white ${msg.type === 'user' ? 'bg-accent' : 'bg-neutral border border-gray-500'}`}>
-              {msg.text}
-            </span>
-          </div>
-        ))}
+          {/* Messages will be dynamically inserted here */}
+          {messages.map((msg, index) => (
+            <div key={index} className={`chat-message ${msg.type === 'user' ? 'text-right' : ''}`}>
+              <span className={`inline-block px-3 py-2 rounded-lg max-w-xs m-1 text-white ${msg.type === 'user' ? 'bg-accent' : 'bg-neutral border border-gray-500'}`}>
+                {msg.text}
+              </span>
+            </div>
+          ))}
       </div>
       <div className="mt-4 flex">
         <input
